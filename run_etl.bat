855b0dd3b84c7819191267a12ff9421edc959ab8
Arina Godman <146754998+ArinaGodman@users.noreply.github.com>
2024-06-24 11:01:10 +0200

    batch file changes

--- run_etl.bat
+++ run_etl.bat
@@ -1,3 +1,21 @@
 @echo off
+echo Changing directory to the script location...
+cd /d "C:\Users\lalka\OneDrive\DOCUME~1\GitHub\Conversionista\etl_converstionista"
+if %errorlevel% neq 0 (
+    echo Failed to change directory.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Current directory is:
+cd
+
+echo Running the ETL pipeline...
 python etl_sales_pipeline.py
+if %errorlevel% neq 0 (
+    echo Failed to run the Python script.
+    pause
+    exit /b %errorlevel%
+)
+
 pause
